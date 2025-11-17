275a7e2f04ed7e755317cb00bf0a8939e9cc0bfd
Anthony PRIGENT <aprigent@hellowork.com>
2025-11-17 17:27:55 +0100

    Ajout du site généré Apollinaire en Marinière

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://apollinaire-en-mariniere.fr',
+  integrations: [mdx(), sitemap(), icon()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
